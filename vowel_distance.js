31e2e850f8b878ff20bf5e4dd4720ce20a9351f0
ayush8116-tech <vermajiii8116@gmail.com>
2025-09-28 22:54:38 +0530

    Update vowel_distance.js

--- vowel_distance.js
+++ vowel_distance.js
@@ -1,49 +1,42 @@
 function vowelSpot(string, index) {
-  const isVowel = string[index] === "a" || string[index] === "e" || string[index] === "i" || string[index] === "o" || string[index] === "u;"
+  const isVowel = string[index] === "a" || string[index] === "e" || string[index] === "i" || string[index] === "o" || string[index] === "u";
   return isVowel;
 }
 
 function vowelDifference(string) {
-  let index = 0;
   let count = 0;
   let vowelIndex = 0;
   let difference = 0;
-  let prevDifference;
+  let prevDifference = 1001;
   let minDiff;
-  while(index < string.length  ){
-    
-    if(vowelSpot(string, index)){
+  let index = 0;
+
+  while (index < string.length) {
+    if (vowelSpot(string, index)) {
       count = count + 1;
-      console.log(count);
-      
-      if (count > 1) { 
+
+      if (count > 1) {
         difference = index - vowelIndex;
-        minDiff = difference < prevDifference && count > 2 ? difference : prevDifference;
+        minDiff = (difference < prevDifference) ? difference : prevDifference;
         prevDifference = difference;
-        console.log("ja rha hai");
-        
       }
       vowelIndex = index;
-      console.log(32);
-      
     }
-    
-   index = index + 1;
-
+    index = index + 1;
   }
 
-  
-  
-  return difference;
+  if (count < 2) {
+    return -1;
+  }
+
+  return minDiff;
 }
 
 function resultSymbol(actualValue, expectedValue) {
-
   const isEqual = actualValue === expectedValue;
   const symbol = isEqual ? "✅" : "❌";
 
   return symbol;
-
 }
 
 function testCase(string, expectedValue) {
@@ -54,11 +47,18 @@ function testCase(string, expectedValue) {
   const message = symbolMessage + expectedValueMessage + actualValueMessage;
 
   console.log(message);
-
 }
+
 function testOfAll() {
   testCase("aa", 1);
-
-  testCase("hll", -1)
+  testCase("aba", 2);
+  testCase("abcdegi", 2);
+  testCase("hello", 3);
+  testCase("strength", -1);
+  testCase("apple", 4);
+  testCase("aapa", 1);
+  testCase("abcejhupoccccci", 2);
+  testCase("beauty", 1);
 }
+
 testOfAll();
