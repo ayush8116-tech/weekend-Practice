d2a2723dada5f161aa7b6255609245268d5aa118
ayush8116-tech <vermajiii8116@gmail.com>
2025-10-04 22:05:06 +0530

    Create printing_permutation_string.js

---
+++ printing_permutation_string.js
@@ -0,0 +1,26 @@
+function permutations(string){
+  return '"","a"';
+}
+
+function resultSymbol(actualValue, expectedValue) {
+  const isEqual = actualValue === expectedValue;
+  const symbol = isEqual ? "✅" : "❌";
+
+  return symbol;
+}
+
+function testCase(string, expectedValue) {
+  const actualValue = permutations(string);
+  const expected = `expected : ${expectedValue}`;
+  const actual = `actual : ${actualValue}`;
+  const symbol = resultSymbol(actualValue, expectedValue);
+  const message = `${symbol} || ${expected} || ${actual}`;
+
+  console.log(message);
+}
+
+function testOfAll() {
+ testCase("a", '"","a"');
+}
+
+testOfAll();
