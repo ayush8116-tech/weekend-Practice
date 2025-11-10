a6caf962313260b4f495d39c95cbff5ef1e8e848
Ayush Verma <ayushverma@Ayushs-MacBook-Pro.local>
2025-11-10 22:14:01 +0530

    made for task 3

--- number_sequence_analyzer.js
+++ number_sequence_analyzer.js
@@ -16,24 +16,29 @@
 // Return the difference between the largest and smallest number.
 // 👉 Output: 9 - 1 = 8
 
+//######################    START FROM HERE     ###########################//
+
+// Find the number that appears most frequently.
+// 👉 Output example: 9
 const nums = [3, 9, 2, 9, 5, 2, 9, 1, 5, ];
 
 const occurences = (repetitionLogs, value) => {
   const index = repetitionLogs.findIndex(row => row[0] === value);
-
+  
   if(index === -1) {
     repetitionLogs.push([value, 1]);
     
     return repetitionLogs;
   }
-
+  
   repetitionLogs[index][1] += 1;
-
-  return repetitionLogs
+  
+  return repetitionLogs;
 }
 
 const frequencyTable = nums.reduce(occurences, []);
-// console.log(frequencyTable);
+// Return a new array of numbers that appear more than once, sorted in ascending order.
+// 👉 Output: [2, 5, 9]
 
 const maxFrequency = frequencyTable.reduce(((max, row) =>{
   // console.log(max);
@@ -56,16 +61,32 @@ const mostFrequent = frequencyTable.reduce(((frequentElements, row) => {
 
 console.log(mostFrequent);
 
-// [ [ 3, 1 ], [ 9, 3 ], [ 2, 3 ], [ 5, 2 ], [ 1, 1 ] ]
+// Return a new array of numbers that appear more than once, sorted in ascending order.
+// 👉 Output: [2, 5, 9]
 
 const repeatMoreThanOnce = frequencyTable.reduce(((atleastTwoOccurence, row) => {
   if(row[1] > 1) {
     atleastTwoOccurence.push(row[0])
     return atleastTwoOccurence;
   }
-
+  
   return atleastTwoOccurence;
 }), [])
 
 console.log("data which repeat more than once :",repeatMoreThanOnce.sort((x, y) => x - y));
+
+// [ [ 3, 1 ], [ 9, 3 ], [ 2, 3 ], [ 5, 2 ], [ 1, 1 ] ]
+
+// Return the sum of all unique numbers (that appear exactly once).
+// 👉 Output: 1 + 3 = 4
+
+const distinct = frequencyTable.map(row => row[0]);
+// console.log("distinct :",distinct);
+
+const sumOfData = distinct.reduce((sum, number) => sum + number);
+
+console.log("sum of unique data :", sumOfData);
+
+
+
 
