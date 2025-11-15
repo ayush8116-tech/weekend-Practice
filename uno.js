999e0d68df6b5944285f58214c26713b819630b6
Ayush Verma <ayushverma@Ayushs-MacBook-Pro.local>
2025-11-15 23:48:48 +0530

    player cards are distributed

--- uno.js
+++ uno.js
@@ -1,3 +1,5 @@
+const dbg = (x) => (console.log(x)) && x;
+
 const mainDeck = [
     "Blue 0",
     "Blue 1",
@@ -109,316 +111,40 @@ const mainDeck = [
     "Wild Draw-Four",
 ];
 
-function removingCardsFromDeck(randomIndex, deck) {
-    const deck1 = deck.slice(0, randomIndex);
-    const deck2 = deck.slice(randomIndex + 1);
+const structuredDeck = mainDeck.map((card) => {
+    const object = {};
 
-    return deck1.concat(deck2);
-}
+    object.color = card.split(" ")[0];
+    if (!isNaN(parseInt(card.split(" ")[1]))) {
+        object.number = card.split(" ")[1];
+        return object;
+    }
 
-function randomIndex(maxIndex) {
-    return Math.floor(Math.random() * maxIndex);
-}
+    object.power = card.split(" ")[1];
+    return object;
+});
 
-function cardDistribution(mainDeck) {
-    const userHandCards = [];
-    const botHandCards = [];
+const powerCards = structuredDeck.filter((card) => card.power);
+const normalCards = structuredDeck.filter((card) => card.number);
+
+const random = (x) => Math.floor(Math.random() * x);
+
+const distribution = (deck) => {
+    const distributed = [];
 
     for (let i = 0; i < 7; i++) {
-        const cardIndex = randomIndex(mainDeck.length);
-        const card = mainDeck[cardIndex];
-        userHandCards.push(card);
-
-        mainDeck = removingCardsFromDeck(cardIndex, mainDeck);
+        const index = random(deck.length);
+        distributed.push(deck[index]);
+        deck.splice(index, 1);
     }
 
-    for (let i = 0; i < 7; i++) {
-        const cardIndex = randomIndex(mainDeck.length);
-        const card = mainDeck[cardIndex];
-        botHandCards.push(card);
+    return distributed;
+};
 
-        mainDeck = removingCardsFromDeck(cardIndex, mainDeck);
-    }
+const playerOneCards = distribution(mainDeck); 
+const discardPile = [];
 
-    return [userHandCards, botHandCards, mainDeck];
-}
+const openFirstCard = 
 
-console.clear();
-
-const allDistributedCards = cardDistribution(mainDeck);
-let userCards = allDistributedCards[0];
-
-console.log(userCards.join("\n"));
-console.log("\n\n");
-
-let botCards = allDistributedCards[1];
-console.log(botCards.join("\n"));
-
-let deck = allDistributedCards[2];
-
-function cardReveal(deck) {
-    const index = randomIndex(deck.length);
-    const revealedCard = deck[index];
-    deck = removingCardsFromDeck(index, deck);
-
-    console.log("\n\n");
-
-    //   console.log(revealedCard);
-    //   console.log(deck.length);
-    return [revealedCard, deck];
-}
-
-function isCardAvailable(cardInDiscardPile, userCards) {
-    const discardedCardArray = cardInDiscardPile.split(" ");
-
-    const colour = discardedCardArray[0];
-    const cardPower = discardedCardArray[1];
-
-    for (let cardIndex = 0; cardIndex < userCards.length; cardIndex++) {
-        if (
-            userCards[cardIndex].includes(colour) ||
-            userCards[cardIndex].includes(cardPower) ||
-            discardedCardArray.includes("Wild")
-        ) {
-            return true;
-        }
-    }
-    return false;
-}
-
-function isCardPlayable(cardInDiscardPile, drawnCard) {
-    const drawnCardArray = drawnCard.split(" ");
-
-    const colour = drawnCardArray[0];
-    const cardPower = drawnCardArray[1];
-
-    if (
-        cardInDiscardPile.includes(colour) ||
-        cardInDiscardPile.includes(cardPower) || drawnCardArray.includes("Wild")
-    ) {
-        return true;
-    }
-
-    return false;
-}
-// console.log(mainDeck.length);
-
-const cards = cardReveal(deck);
-let cardInDiscardPile = cards[0];
-deck = cards[1];
-
-function isCardDrawn(deck, isDrawTwo, isDrawFour) {
-    const drawnCards = [];
-
-               if(isDrawTwo) {
-                for (let i = 0; i < 2; i++) {
-                    const revealedCardArray = cardReveal(deck);
-                    drawnCards.push(revealedCardArray[0]);
-                    deck = revealedCardArray[1];
-                }
-                
-                return [true, drawnCards];
-               }
-               
-               if(isDrawFour) {
-                for (let i = 0; i < 4; i++) {
-                    const revealedCardArray = cardReveal(deck);
-                    drawnCards.push(revealedCardArray[0]);
-                    deck = revealedCardArray[1];
-                }
-                return [true, drawnCards];
-               }
-
-}
-
-let cardDrawnOrNot = false;
-
-function userTurn() {
-    console.log(cardInDiscardPile);
-    let drawnCard;
-    const isDrawTwo = cardInDiscardPile.includes("Draw-Two");
-    const isDrawFour = cardInDiscardPile.includes("Draw-Four");
-    const isCardInHand = isCardAvailable(cardInDiscardPile, userCards);
-
-    if (!isCardInHand || isDrawTwo || isDrawFour) {
-            if(cardDrawnOrNot === false) {
-                const cardDrawnAfterPowerCard = isCardDrawn(deck, isDrawTwo, isDrawFour);
-                userCards = userCards.push(cardDrawnAfterPowerCard[1]);
-                cardDrawnOrNot = cardDrawnAfterPowerCard[0];
-                return;
-            } else {
-                cardDrawnOrNot = false;
-            }
-
-        console.log(`${cardInDiscardPile} is not available in your hand \n draw it`);
-
-        const askToDraw = prompt("Draw (enter any number) : ");
-
-        if (parseInt(askToDraw) < Infinity) {
-            // console.log(deck.length);
-            drawnCard = (cardReveal(deck))[0];
-            deck = cardReveal(deck)[1];
-        }
-
-        console.log(`drawn card is ${drawnCard}`);
-
-        if (isCardPlayable(cardInDiscardPile, drawnCard)) {
-            const askToPlay = prompt("1.PLAY / 2.KEEP");
-
-            if (parseInt(askToPlay) === 1) {
-                cardInDiscardPile = drawnCard;
-            } else {
-                userCards.push(drawnCard);
-            }
-        } else {
-            userCards.push(drawnCard);
-        }
-        console.log(userCards);
-    } else {
-        const askToPlay = prompt("PLAY (enter any number): ");
-
-        if (parseInt(askToPlay) < Infinity) {
-            const cardsPlayable = [];
-
-            for (let i = 0; i < userCards.length; i++) {
-                const cardInDiscardPileArray = cardInDiscardPile.split(" ");
-
-                if (
-                    userCards[i].includes(cardInDiscardPileArray[0]) ||
-                    userCards[i].includes(cardInDiscardPileArray[1])
-                ) {
-                    cardsPlayable.push(userCards[i]);
-                }
-            }
-
-            const cardsForPlay = cardsPlayable;
-            console.log(`You can play these cards \n ${cardsForPlay}`);
-
-            const cardToPlay = prompt(
-                "CHOOSE CARD TO PLAY (enter the number) :  ",
-            );
-            userCards = removingCardsFromDeck(
-                userCards.indexOf(cardsForPlay[cardToPlay - 1]),
-                userCards
-            );
-
-            cardInDiscardPile = cardsForPlay[cardToPlay - 1];
-            console.log(`now discard pile card is ${cardInDiscardPile}`);
-        } else {
-            userCards.push(cardReveal(deck));
-            console.log(userCards);
-        }
-    }
-}
-
-function botTurn() {
-    console.log(cardInDiscardPile);
-    let drawnCard;
-    const isDrawTwo = cardInDiscardPile.includes("Draw-Two");
-    const isDrawFour = cardInDiscardPile.includes("Draw-Four");
-    const isCardInHand = isCardAvailable(cardInDiscardPile, userCards);
-
-    if (!isCardInHand || isDrawTwo || isDrawFour) {
-           if(cardDrawnOrNot === false) {
-                const cardDrawnAfterPowerCard = isCardDrawn(deck, isDrawTwo, isDrawFour);
-                botCards = botCards.push(cardDrawnAfterPowerCard[1]);
-                cardDrawnOrNot = cardDrawnAfterPowerCard[0];
-                return;
-            } else {
-                cardDrawnOrNot = false;
-            }
-
-
-        console.log(
-            `${cardInDiscardPile} is not available in your hand \n draw it`,
-        );
-        
-        const askToDraw = prompt("Draw (enter any number) : ");
-        if (parseInt(askToDraw) < Infinity) {
-            drawnCard = (cardReveal(deck))[0];
-            deck = (cardReveal(deck))[1];
-            // console.log(deck.length);
-        }
-
-        console.log(`drawn card is ${drawnCard}`);
-
-        if (isCardPlayable(cardInDiscardPile, drawnCard)) {
-            const askToPlay = prompt("1.PLAY / 2.KEEP");
-
-            if (parseInt(askToPlay) === 1) {
-                cardInDiscardPile = drawnCard;
-            } else {
-                botCards.push(drawnCard);
-            }
-        } else {
-            botCards.push(drawnCard);
-        }
-        console.log(botCards);
-    } else {
-        const askToPlay = prompt("PLAY (enter any number): ");
-
-        if (parseInt(askToPlay) < Infinity) {
-            const cardsPlayable = [];
-
-            for (let i = 0; i < botCards.length; i++) {
-                const cardInDiscardPileArray = cardInDiscardPile.split(" ");
-
-                if (
-                    botCards[i].includes(cardInDiscardPileArray[0]) ||
-                    botCards[i].includes(cardInDiscardPileArray[1])
-                ) {
-                    cardsPlayable.push(botCards[i]);
-                }
-            }
-
-            const cardsForPlay = cardsPlayable;
-            console.log(`You can play these cards \n ${cardsForPlay}`);
-
-            const cardToPlay = prompt(
-                "CHOOSE CARD TO PLAY (enter the number) :  ",
-            );
-            botCards = removingCardsFromDeck(
-                botCards.indexOf(cardsForPlay[cardToPlay - 1]),
-                botCards,
-            );
-
-            cardInDiscardPile = cardsForPlay[cardToPlay - 1];
-            console.log(`now discard pile card is ${cardInDiscardPile}`);
-        } else {
-            botCards.push(cardReveal(deck));
-            console.log(botCards);
-        }
-    }
-}
-
-function playingGame() {
-    let i = 0;
-    while (userCards.length !== 0 && botCards.length !== 0) {
-        const remainder = i % 2;
-        console.log(remainder);
-
-        if (remainder === 0) {
-            console.clear();
-            console.log("USER TURN");
-            console.log(`length of deck is ${deck.length}`);
-            console.log(`bot has ${botCards.length} cards in hand.`);
-            console.log(userCards);
-            
-            userTurn();
-        } else {
-            console.clear();
-            console.log("BOT TURN");
-            console.log(`length of deck is ${deck.length}`);
-            console.log(`user has ${userCards.length} cards in hand.`);
-            console.log(botCards);
-
-            botTurn();
-        }
-        i = i + 1;
-    }
-
-    const result = userCards.length === 0 ? "USER WINS !!" : "BOT WINS !!";
-    console.log(result);
-}
-
-playingGame();
+dbg(playerOneCards)
+dbg(mainDeck.length)
